4c9a0df7bbb5e471199642c97b6928d6d8254d9a
JerzyMaj96 <jerzy.maj96@gmail.com>
2025-06-07 01:30:26 +0200

    Unused imports deleted

--- src/main/java/com/jerzymaj/budgetmanagement/budget_management_app/controllers/MonthlyCostsSummaryController.java
+++ src/main/java/com/jerzymaj/budgetmanagement/budget_management_app/controllers/MonthlyCostsSummaryController.java
@@ -9,11 +9,8 @@ import com.jerzymaj.budgetmanagement.budget_management_app.models.MonthlyCostsSu
 import com.jerzymaj.budgetmanagement.budget_management_app.services.GPTService;
 import com.jerzymaj.budgetmanagement.budget_management_app.services.MonthlyCostsService;
 import com.jerzymaj.budgetmanagement.budget_management_app.services.UserService;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.math.BigDecimal;
 
