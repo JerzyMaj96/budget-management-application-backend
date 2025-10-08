6024e6b4e35c4dc2ab0299352406abc659864cbd
JerzyMaj96 <jerzy.maj96@gmail.com>
2025-10-08 16:29:29 +0200

    Add RestTemplate bean and import .env properties
    Introduced a RestTemplate bean in CorsConfig for HTTP client needs. Updated application.properties to optionally import environment variables from a .env file for improved configuration flexibility.

--- src/main/java/com/jerzymaj/budgetmanagement/budget_management_app/configuration/CorsConfig.java
+++ src/main/java/com/jerzymaj/budgetmanagement/budget_management_app/configuration/CorsConfig.java
@@ -2,6 +2,7 @@ package com.jerzymaj.budgetmanagement.budget_management_app.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -21,5 +22,10 @@ public class CorsConfig {
                         .maxAge(3600);
             }
         };
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 }
